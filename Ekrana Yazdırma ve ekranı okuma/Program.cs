4e2f161b15ec8b28fe9e0ebe830b82abc64a648c
Sekvan Dayan <sekvandyn56@gmail.com>
2024-12-06 18:32:56 +0300

    udemy-c-sharp-programlama-dili-her-seviyeye-hitap-eden-egitim-seti-kursundan-ogrenip-yazdigim-c-sharp-kodlari
    yazdığım c# kodları

---
+++ Ekrana Yazdırma ve ekranı okuma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ekrana_Yazdırma_ve_ekranı_okuma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //yorum satırı kodu etkilemez açıklamalar yapmanı sağşlar satır başına // ekleyerek yazılabilir
+            /* veya bu şekilde birden fazla satırı yoruma dönüştürebilirsiniz */
+            Console.WriteLine("C# Kodlarının ilki");//konsola yazdırır
+            Console.WriteLine("Writeline alt satıra yazdırılır");// writeline ile iki farklı yazdırma olunca alt alta yazdırılır
+
+            Console.Write("Write ise ");
+            Console.Write("Yan yana yazdırır");// write ile iki farklı yazdırma olunca yanyana yazdırılır
+            Console.ReadLine();//konsolun okunabilmesi için ekranda kalmasını sağlar
+        }
+    }
+}
---
+++ Ekrana Yazdırma ve ekranı okuma/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ekrana Yazdırma ve ekranı okuma")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ekrana Yazdırma ve ekranı okuma")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3f0e4ba5-bcf7-45c6-8456-95ff2525861f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
